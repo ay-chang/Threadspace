c218f559b6976847763785e5f0a7a2908a624559
AllenChang7 <allenychang2003@gmail.com>
2025-11-08 23:21:43 -0500

    feat: empty projects component

---
+++ web/src/components/dashboard/EmptyProjects.tsx
@@ -0,0 +1,37 @@
+import { ArrowUpRightIcon, FolderPen } from "lucide-react";
+import { Button } from "../ui/button";
+import {
+    Empty,
+    EmptyHeader,
+    EmptyMedia,
+    EmptyTitle,
+    EmptyDescription,
+    EmptyContent,
+} from "../ui/empty";
+
+export default function EmptyProjects() {
+    return (
+        <Empty className="from-muted/50 to-background h-full bg-linear-to-b from-30">
+            <EmptyHeader>
+                <EmptyMedia variant="icon">
+                    <FolderPen />
+                </EmptyMedia>
+                <EmptyTitle>No Projects Yet</EmptyTitle>
+                <EmptyDescription>
+                    You haven&apos;t created any projects yet. Get started by creating your
+                    first project.
+                </EmptyDescription>
+            </EmptyHeader>
+            <EmptyContent>
+                <div className="flex gap-2">
+                    <Button>Create Project</Button>
+                </div>
+            </EmptyContent>
+            <Button variant="link" asChild className="text-muted-foreground" size="sm">
+                <a href="#">
+                    Learn More <ArrowUpRightIcon />
+                </a>
+            </Button>
+        </Empty>
+    );
+}
